11482ab5c8910aed0ff06452a484b9c44b7bb3f5
nabarun <nabarun@deepstacksoft.com>
2022-04-26 12:27:02 +0530

    Add commands for auction operations

---
+++ src/cmds/cns-cmds/auction-cmds/bid.ts
@@ -0,0 +1,15 @@
+import yargs from 'yargs';
+
+export const command = 'bid';
+
+export const desc = 'Auction bid operations.';
+
+exports.builder = (yargs: yargs.Argv) => {
+  return yargs.options({
+      'auction-id': { type: 'string' },
+      'type': { type: 'string' },
+      'quantity': { type: 'string' },
+      'file-path': { type: 'string' }
+    }).commandDir('bid-cmds')
+    .demandCommand();
+}
---
+++ src/cmds/cns-cmds/auction.ts
@@ -0,0 +1,10 @@
+import yargs from 'yargs';
+
+export const command = 'auction';
+
+export const desc = 'Auction operations.';
+
+exports.builder = (yargs: yargs.Argv) => {
+  return yargs.commandDir('auction-cmds')
+    .demandCommand();
+}
