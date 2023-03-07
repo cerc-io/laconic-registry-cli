3258e4cfe330d9abe3c60cd3d43684657cd99f73
0xmuralik <krishna192reddy@gmail.com>
2023-03-07 16:27:53 +0530

    get bond id

--- src/cmds/cns-cmds/bond-cmds/create.ts
+++ src/cmds/cns-cmds/bond-cmds/create.ts
@@ -34,6 +34,7 @@ export const handler = async (argv: Arguments) => {
 
   const registry = new Registry(gqlEndpoint, restEndpoint, chainId);
   const fee = getGasAndFees(argv, cnsConfig);
+  const bondId = await registry.getNextBondId(privateKey);
   const result = await registry.createBond({ denom, amount }, privateKey, fee);
-  console.log(verbose ? JSON.stringify(result, undefined, 2) : result.data);
+  console.log(true ? JSON.stringify(result, undefined, 2)+"\nBondId:"+bondId : bondId);
 }
