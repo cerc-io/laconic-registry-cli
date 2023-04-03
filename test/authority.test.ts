679313d9605ecb167c05632247b9bfee8035fdb4
0xmuralik <krishna192reddy@gmail.com>
2023-04-03 16:40:32 +0530

    authority tests

--- test/authority.test.ts
+++ test/authority.test.ts
@@ -1,7 +1,7 @@
 import {cliTest,createBond} from './helper';
 
 const args= "authority "
-const name="laconic"
+const name=`laconic-${Date.now()}`
 
 var bondId: string;
 
@@ -15,16 +15,27 @@ describe("test authority",() => {
 
     it("reserve authority",async ()=>{
         const resp=cliTest(args+"reserve "+name);
-        expect(resp).toBeDefined;
+        expect(resp).toBeDefined();
+        expect(resp.success).toBeTruthy();
     });
 
     it("lookup authority information.",async ()=>{
         const resp=cliTest(args+"whois "+ name);
-        expect(resp).toBeDefined;
+        expect(resp).toBeDefined();
+        expect(resp.length).toEqual(1);
+        expect(resp[0].status).toEqual("active")
+        expect(resp[0].bondId).toEqual("")
     });
 
     it("set authority bond",async ()=>{
-        const resp=cliTest(args+"bond set "+name+" "+bondId);
-        expect(resp).toBeDefined;
+        const bondSet=cliTest(args+"bond set "+name+" "+bondId);
+        expect(bondSet).toBeDefined();
+        expect(bondSet.success).toBeTruthy();
+
+        const resp=cliTest(args+"whois "+ name);
+        expect(resp).toBeDefined();
+        expect(resp.length).toEqual(1);
+        expect(resp[0].status).toEqual("active")
+        expect(resp[0].bondId).toEqual(bondId)
     });
 });
