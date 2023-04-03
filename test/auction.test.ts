9a45237b2dae72cb504e0ce141a699e372783919
0xmuralik <krishna192reddy@gmail.com>
2023-04-03 12:58:22 +0530

    auction tests

--- test/auction.test.ts
+++ test/auction.test.ts
@@ -5,7 +5,7 @@ jest.setTimeout(3 * 60 * 1000);
 const args= "auction ";
 const quantity=25000000
 const type="aphoton"
-const name="laconic-auction"
+const name=`laconic-auction-${Date.now()}`
 
 
 var auctionId: string;
@@ -22,11 +22,25 @@ if (!process.env.TEST_AUCTIONS_ENABLED) {
            auctionId = getAuctionId(name)
         });
     
+        it("get auction",async ()=>{
+            const resp=cliTest(args+"get "+auctionId);
+            expect(resp).toBeDefined();
+            expect(resp.length).toEqual(1);
+            expect(resp[0].id).toEqual(auctionId)
+        });
+
         it("bid commit",async ()=>{
             const resp=cliTest(args+"bid commit "+auctionId+" "+quantity+" "+type);
-            expect(resp).toBeDefined;
+            expect(resp).toBeDefined();
+            expect(resp.reveal_file).toBeDefined();
     
             filepath = resp.reveal_file
+
+            const auction=cliTest(args+"get "+auctionId);
+            expect(auction).toBeDefined;
+            expect(auction.length).toEqual(1);
+            expect(auction[0].bids.length).toEqual(1);
+            expect(auction[0].bids[0].bidAmount.quantity).toEqual("0")
         });
     
         it("Wait for reveal phase.", (done) => {
@@ -35,12 +49,14 @@ if (!process.env.TEST_AUCTIONS_ENABLED) {
     
         it("bid reveal", async ()=>{
             const resp=cliTest(args+"bid reveal "+auctionId+" "+filepath);
-            expect(resp).toBeDefined;
-        });
-    
-        it("get auction",async ()=>{
-            const resp=cliTest(args+"get "+auctionId);
-            expect(resp).toBeDefined;
+            expect(resp).toBeDefined();
+            expect(resp.success).toBeTruthy();
+
+            const auction=cliTest(args+"get "+auctionId);
+            expect(auction).toBeDefined;
+            expect(auction.length).toEqual(1);
+            expect(auction[0].bids.length).toEqual(1);
+            expect(auction[0].bids[0].bidAmount.quantity).toEqual(quantity+"")
         });
     });
   }
