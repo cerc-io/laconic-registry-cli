936ad73a8932a488252ad355eb7fe3ec8ef1051d
Prathamesh Musale <prathamesh@noreply.git.vdb.to>
2024-10-08 03:47:11 +0000

    Use registry-sdk methods to parse gas and fees (#85)
    Part of [Service provider auctions for web deployments](https://www.notion.so/Service-provider-auctions-for-web-deployments-104a6b22d47280dbad51d28aa3a91d75)
    
    Co-authored-by: IshaVenikar <ishavenikar7@gmail.com>
    Reviewed-on: https://git.vdb.to/cerc-io/laconic-registry-cli/pulls/85
    Co-authored-by: Prathamesh Musale <prathamesh@noreply.git.vdb.to>
    Co-committed-by: Prathamesh Musale <prathamesh@noreply.git.vdb.to>

--- src/util/fees.ts
+++ src/util/fees.ts
@@ -1,28 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import assert from 'assert';
 import { Arguments } from 'yargs';
 
-import { StdFee, GasPrice, parseCoins } from '@cosmjs/stargate';
-
-export const parseGasAndFees = (gas?: string, fees?: string): StdFee | number | undefined => {
-  // If fees is not given or a number, treat it as a gas estimation multiplier
-  if (fees === null || fees === undefined) {
-    return undefined;
-  }
-
-  const isFeesANumber = !isNaN(Number(fees));
-  if (isFeesANumber) {
-    return Number(fees);
-  }
-
-  // If fees is not a gas estimation multiplier, gas is required
-  assert(gas, 'Invalid gas.');
-
-  return {
-    amount: parseCoins(String(fees)),
-    gas: String(gas)
-  };
-};
+import { parseGasAndFees, getGasPrice as registryGetGasPrice } from '@cerc-io/registry-sdk';
+import { StdFee, GasPrice } from '@cosmjs/stargate';
 
 export const getGasAndFees = (argv: Arguments, config: any = {}): StdFee | number | undefined => {
   return parseGasAndFees(
@@ -33,5 +13,5 @@ export const getGasAndFees = (argv: Arguments, config: any = {}): StdFee | numbe
 
 export const getGasPrice = (argv: Arguments, config: any = {}): GasPrice | undefined => {
   const gasPriceString = argv.gasPrice || config.gasPrice;
-  return gasPriceString != null ? GasPrice.fromString(String(gasPriceString)) : undefined;
+  return registryGetGasPrice(gasPriceString);
 };
